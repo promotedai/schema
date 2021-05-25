841294aa12ef3e5c096e2f6fe16b16faa4c1255c
Dan Hill <dhill@promoted.ai>
2021-05-25 08:49:42 -0700

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='schema',
+    version='1.0.1',
+    description='Core schema',
+    long_description=readme,
+    author='Dan Hill',
+    author_email='dhill@promoted.ai',
+    url='https://github.com/promotedai/schema',
+    package_dir={'': 'generated'},
+    packages=find_packages('generated'),
+    install_requires=[],
+    package_data={
+        '': ['*.pyi', 'py.typed', '*/*.pyi', '*/*/*.pyi'],
+    },
+    include_package_data=True,
+)
