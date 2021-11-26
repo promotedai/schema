fc72de956f6f7ef828d2aefdca30aa9764abb19c
Scott McMaster <scott@promoted.ai>
2021-11-26 10:22:50 -0800

    Make ExecutionServerVersion public

--- generated/py/proto/delivery/execution_pb2.pyi
+++ generated/py/proto/delivery/execution_pb2.pyi
@@ -72,16 +72,23 @@ global___DeliveryLog = DeliveryLog
 
 class DeliveryExecution(google.protobuf.message.Message):
     """Contains the inner execution details for a Delivery call.
-    Next ID = 7.
+    Next ID = 8.
     """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     EXECUTION_SERVER_FIELD_NUMBER: builtins.int
+    EXECUTIONSERVERVERSION_FIELD_NUMBER: builtins.int
     execution_server: global___ExecutionServer.V = ...
     """Where delivery happened, i.e. via the SDK or some approach on the API side."""
+
+    ExecutionServerVersion: typing.Text = ...
+    """Version of the execution server, for example a commit hash
+    or public repo release version.
+    """
 
     def __init__(self,
         *,
         execution_server : global___ExecutionServer.V = ...,
+        ExecutionServerVersion : typing.Text = ...,
         ) -> None: ...
-    def ClearField(self, field_name: typing_extensions.Literal["execution_server",b"execution_server"]) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["ExecutionServerVersion",b"ExecutionServerVersion","execution_server",b"execution_server"]) -> None: ...
 global___DeliveryExecution = DeliveryExecution
