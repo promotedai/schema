99adb35ffaaec2e2e0b9be968a826e8771d1cf49
Dan Hill <dhill@promoted.ai>
2022-04-12 22:40:06 -0700

    refactor: remove dependabot. change comments

--- generated/py/proto/delivery/delivery_pb2.pyi
+++ generated/py/proto/delivery/delivery_pb2.pyi
@@ -324,9 +324,9 @@ class Insertion(google.protobuf.message.Message):
     """Optional.  We'll look this up using the external_content_id."""
 
     position: builtins.int = ...
-    """Optional. 0-based. Position "in what" depends on insertion context:
-    if request_insertion, then position provided by client or retrieval
-    if response_insertion, then the position returned by Delivery to the client
+    """Optional. 0-based.  Position is the global position across pages.
+    E.g. the first item on the 4th page where page_size=10 is position=40.
+    Do not set this on request insertions.  Instead, set retrieval_rank.
     """
 
     @property
